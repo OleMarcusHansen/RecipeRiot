daed76d6bf5f556b1660a93edb27b3c3f8edcf49
Ole Marcus Hansen <omhanse@hiof.no>
2023-10-11 13:53:34 +0200

    add home screen prototype

--- app/src/main/java/no/hiof/reciperiot/ui/theme/HomeScreen.kt
+++ app/src/main/java/no/hiof/reciperiot/ui/theme/HomeScreen.kt
@@ -1,8 +1,80 @@
 package no.hiof.reciperiot.ui.theme
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
-fun HomeScreen() {
+fun HomeScreen(modifier: Modifier = Modifier) {
+    val time = remember { mutableStateOf("") }
+    val paprika = remember { mutableStateOf(false) }
 
+    Column(modifier = modifier
+        .padding(horizontal = 50.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)) {
+        Text("Valg", fontSize = 20.sp)
+        TimeInput(text = "Time in minutes ", state = time)
+        Text("Dine ingredienser", fontSize = 20.sp)
+        Ingredient(text = "Paprika", state = paprika)
+        Column(modifier = Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+            Button(onClick = { /*ChatGPT*/ }) {
+                Text("Generer oppskrift")
+            }
+        }
+    }
+}
+
+@Composable
+fun TimeInput(text : String, state : MutableState<String>){
+    Row(){
+        Text(text)
+        BasicTextField(value = state.value,
+            onValueChange = {newText -> state.value = newText},
+            textStyle = TextStyle(fontSize = 16.sp),
+            modifier = Modifier
+                .background(Color.White)
+                .border(1.dp, Color.Gray),
+            keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Number)
+        )
+    }
+}
+
+@Composable
+fun Ingredient(text : String, state : MutableState<Boolean>){
+    Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceAround){
+        Text(text)
+        Checkbox(
+            checked = state.value,
+            onCheckedChange = { newValue ->
+                state.value = newValue
+            },
+            modifier = Modifier.size(24.dp)
+        )
+    }
 }
