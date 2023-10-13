0aab962a0d3e29c6765d521213fb979a550c091d
tsvar <galiomain@outlook.com>
2023-10-13 13:09:01 +0200

    recipecard

--- app/src/main/java/no/hiof/reciperiot/ui/theme/Favourites.kt
+++ app/src/main/java/no/hiof/reciperiot/ui/theme/Favourites.kt
@@ -1,27 +1,40 @@
 package no.hiof.reciperiot.ui.theme
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import no.hiof.reciperiot.R
 import no.hiof.reciperiot.ui.theme.data.RecipeSource
+import no.hiof.reciperiot.ui.theme.model.Recipe
 
 @Composable
 fun FavouriteMeals() {
     val recipes = RecipeSource().loadRecipes()
+
     Card {
         Box(
             modifier = Modifier
@@ -30,7 +43,7 @@ fun FavouriteMeals() {
             contentAlignment = Alignment.Center
         ) {
             Image(
-                painterResource(R.drawable.food),
+                painter = painterResource(R.drawable.food),
                 contentDescription = "",
                 contentScale = ContentScale.Crop,
                 modifier = Modifier.fillMaxSize()
@@ -42,6 +55,41 @@ fun FavouriteMeals() {
 
             )
         }
+    }
+}
+@Composable
+fun recipeCard(
+    recipe: Recipe,
+    modifier: Modifier = Modifier) {
+    Card(modifier = modifier
+        .padding(8.dp)
+        .fillMaxWidth()
+        .wrapContentHeight(),
+        shape = RoundedCornerShape(8.dp),
+        elevation = CardDefaults.cardElevation(8.dp)){
+
+        Column(
+            modifier = Modifier.padding(8.dp)
+        ) {
+            Image(
+                painter = painterResource(id = recipe.imageResourceId),
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(180.dp)
+                    .clip(RoundedCornerShape(8.dp))
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(
+                text = recipe.title,
+                style = MaterialTheme.typography.headlineMedium,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
+            )
+        }
+
+
     }
 }
 
