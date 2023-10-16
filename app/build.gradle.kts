e36d05aa989ed786ad4f0be69b0f20939d331fcc
tsvar <galiomain@outlook.com>
2023-10-16 13:01:44 +0200

    Favourite trenger bare firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,6 +63,7 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:2.7.3")
     implementation("com.google.firebase:firebase-database-ktx:20.2.2")
     implementation("com.google.firebase:firebase-auth-ktx:22.1.2")
+    implementation("com.google.firebase:firebase-common-ktx:20.4.2")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/no/hiof/reciperiot/ui/theme/data/RecipeSource.kt
+++ app/src/main/java/no/hiof/reciperiot/ui/theme/data/RecipeSource.kt
@@ -6,7 +6,8 @@ import no.hiof.reciperiot.ui.theme.model.Recipe
 class RecipeSource() {
     fun loadRecipes(): List<Recipe> {
         return listOf<Recipe>(
-            Recipe(1,"mat", R.drawable.food, "45min", true)
+            Recipe(1,"mat", R.drawable.food, "45min", true),
+            Recipe(2, "pizza", R.drawable.food, "30min", true)
         )
     }
 }
