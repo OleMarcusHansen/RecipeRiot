a4f947ebebf157da87d772dfec01aff615ba988a
Ole Marcus Hansen <omhanse@hiof.no>
2023-10-16 12:31:41 +0200

    add settings screen padding

--- app/src/main/java/no/hiof/reciperiot/ui/theme/SettingsScreen.kt
+++ app/src/main/java/no/hiof/reciperiot/ui/theme/SettingsScreen.kt
@@ -1,7 +1,9 @@
 package no.hiof.reciperiot.ui.theme
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
@@ -10,13 +12,17 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
-fun SettingsScreen(logout: () -> Unit) {
+fun SettingsScreen(logout: () -> Unit, modifier : Modifier = Modifier) {
     var darkTheme by remember { mutableStateOf(false) }
     var otherSetting by remember { mutableStateOf(false) }
 
-    Column(){
+    Column(modifier = modifier
+        .padding(horizontal = 50.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)){
         SwitchSetting(bool = darkTheme, update = {newValue -> darkTheme = newValue}, text = "Dark theme")
         SwitchSetting(bool = otherSetting, update = {newValue -> otherSetting = newValue}, text = "Other setting")
         Button(onClick = { logout() }) {
