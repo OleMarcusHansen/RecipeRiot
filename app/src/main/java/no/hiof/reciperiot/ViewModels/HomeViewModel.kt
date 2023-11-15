451c7cbd59296804ac93cd928a13bafa62380b25
Ole Marcus Hansen <omhanse@hiof.no>
2023-11-15 13:54:18 +0100

    default recipe and try-catch if ingredients and time is invalid

--- app/src/main/java/no/hiof/reciperiot/ViewModels/HomeViewModel.kt
+++ app/src/main/java/no/hiof/reciperiot/ViewModels/HomeViewModel.kt
@@ -136,111 +136,147 @@ class HomeViewModel : ViewModel() {
     val recipes = mutableStateOf(emptyList<Recipe>())
 
     suspend fun generateGPT(client: OkHttpClient, ingredients: List<String>, time: String): List<Recipe> = withContext(Dispatchers.IO) {
-        println("start gpt generate")
-
         val user = Firebase.auth.currentUser
-        //prompt til chatGPT
-        //bør bli justert og testet for å få best mulig resultat
-        val prompt = """I have only the ingredients: ${ingredients}. I have ${time} minutes to make food. Generate a recipe for me. Your output should be in JSON format: {recipe_name: String, recipe_time: String, recipe_instructions: String, recipe_nutrition: Object, recipe_ingredients: Array}"""
 
-        println(prompt)
+        try {
+            if (ingredients.isEmpty() || time == "" || time.toInt() <= 0){
+                println("Ingredients list empty or time is invalid")
+                val defaultRecipe = Recipe(
+                    "iv",
+                    "Empty bowl",
+                    R.drawable.food,
+                    "https://cdn.discordapp.com/attachments/1148561836724207708/1174328461351997492/image.png?ex=6567319b&is=6554bc9b&hm=554c6bc1c7793a83a9bc3f1b72b5f26edda4a14f243d6bbc25eb318170b28347&",
+                    "0",
+                    false,
+                    "Get an empty bowl",
+                    "{\"calories\":0,\"protein\":0,\"carbohydrates\":0,\"fat\":0}",
+                    "[\"Empty bowl\"]",
+                    user!!.uid
+                )
+                return@withContext listOf(defaultRecipe)
+            }
 
-        //kalle chatCompletion api
-        val mediaType: MediaType = "application/json; charset=utf-8".toMediaType()
-        val body = """
+            println("start gpt generate")
+            //prompt til chatGPT
+            //bør bli justert og testet for å få best mulig resultat
+            val prompt = """I have only the ingredients: ${ingredients}. I have ${time} minutes to make food. Generate a recipe for me. Your output should be in JSON format: {recipe_name: String, recipe_time: String, recipe_instructions: String, recipe_nutrition: Object, recipe_ingredients: Array}"""
+
+            println(prompt)
+
+            //kalle chatCompletion api
+            val mediaType: MediaType = "application/json; charset=utf-8".toMediaType()
+            val body = """
         {
             "model": "gpt-3.5-turbo",
             "messages": [{"role": "system", "content": "You are a recipe generator"}, {"role": "user", "content": "${prompt}"}]
         }
     """
-        val postBody: RequestBody = body.toRequestBody(mediaType)
-        val url = "https://api.openai.com/v1/chat/completions"
-        val request : Request = Request.Builder()
-            .url(url)
-            .post(postBody)
-            .addHeader("Authorization", "Bearer " + Secrets.gpt_key)
-            .build()
+            val postBody: RequestBody = body.toRequestBody(mediaType)
+            val url = "https://api.openai.com/v1/chat/completions"
+            val request : Request = Request.Builder()
+                .url(url)
+                .post(postBody)
+                .addHeader("Authorization", "Bearer " + Secrets.gpt_key)
+                .build()
 
-        // Make the API call
-        val response: Response = try {
-            client.newCall(request).await()
-        } catch (exception: IOException) {
-            // Log error and return default timeout recipe
-            Log.e("ChatCompletionError", "Error calling API: ${exception.message}")
+            // Make the API call
+            val response: Response = try {
+                client.newCall(request).await()
+            } catch (exception: IOException) {
+                // Log error and return default timeout recipe
+                Log.e("ChatCompletionError", "Error calling API: ${exception.message}")
+                val defaultRecipe = Recipe(
+                    "ik",
+                    "Burned toast",
+                    R.drawable.food,
+                    "https://cdn.discordapp.com/attachments/1148561836724207708/1172152256683065384/image.png?ex=655f46db&is=654cd1db&hm=2450543bf60afc32ad2c67d54b00328112ba7cd43656abb0d32b34f60d339d98&",
+                    "60",
+                    false,
+                    "Timed out",
+                    "{\"calories\":0,\"protein\":0,\"carbohydrates\":0,\"fat\":0}",
+                    "[\"Bread\"]",
+                    user!!.uid
+                )
+                return@withContext listOf(defaultRecipe)
+            }
+
+            // Handle the response and return the list of recipes
+            if (response.isSuccessful) {
+                val responseString = response.body?.string()
+                println(responseString)
+                if (responseString != null) {
+                    try{
+                        val responseJSON = JSONObject(responseString)
+                        val messageJSON = JSONObject(responseJSON.getJSONArray("choices").getJSONObject(0).getJSONObject("message").getString("content"))
+                        println(messageJSON)
+
+                        // Image creation
+                        //val imageResponse = generateImage(client, messageJSON.getString("recipe_name"))
+                        //val imageResponseURL = JSONObject(imageResponse).getJSONArray("data").getJSONObject(0).getString("url")
+
+                        // Standard image
+                        val imageResponseURL = "https://cdn.discordapp.com/attachments/1148561836724207708/1172157068497666048/image.png?ex=655f4b56&is=654cd656&hm=a296565e26720c460d137ee7941dd195e597378e26b6e77cc7d1320551067ad0&"
+
+                        val recipes = listOf(
+                            Recipe(
+                                "yh",
+                                messageJSON.getString("recipe_name"),
+                                R.drawable.food,
+                                imageResponseURL,
+                                messageJSON.getString("recipe_time"),
+                                false,
+                                messageJSON.getString("recipe_instructions"),
+                                messageJSON.getString("recipe_nutrition"),
+                                messageJSON.getString("recipe_ingredients"),
+                                user!!.uid
+                            )
+                        )
+                        return@withContext recipes
+                    }
+                    catch (exception: JSONException){
+                        Log.e("ChatCompletionError", "Error parsing chat completion to JSON: ${exception.message}")
+                    }
+                }
+            }
+            else{
+                println(response.message)
+                println(response)
+                println(response.body)
+            }
+
+            // Log error and return default failed recipe
+            Log.e("ChatCompletionError", "Error generating recipe")
             val defaultRecipe = Recipe(
-                "ik",
-                "Burned toast",
+                "uh",
+                "Failed tomato soup",
                 R.drawable.food,
-                "https://cdn.discordapp.com/attachments/1148561836724207708/1172152256683065384/image.png?ex=655f46db&is=654cd1db&hm=2450543bf60afc32ad2c67d54b00328112ba7cd43656abb0d32b34f60d339d98&",
+                "https://cdn.discordapp.com/attachments/1148561836724207708/1172151716741906503/image.png?ex=655f465a&is=654cd15a&hm=2ee66b50819a6faa6c8b4e3afa638b5540f1cd59f386703b10b40609ac7645a4&",
                 "60",
                 false,
-                "Timed out",
+                "Something failed",
                 "{\"calories\":0,\"protein\":0,\"carbohydrates\":0,\"fat\":0}",
-                "[\"Bread\"]",
+                "[\"Failure\"]",
                 user!!.uid
             )
             return@withContext listOf(defaultRecipe)
         }
-
-        // Handle the response and return the list of recipes
-        if (response.isSuccessful) {
-            val responseString = response.body?.string()
-            println(responseString)
-            if (responseString != null) {
-                try{
-                val responseJSON = JSONObject(responseString)
-                val messageJSON = JSONObject(responseJSON.getJSONArray("choices").getJSONObject(0).getJSONObject("message").getString("content"))
-                println(messageJSON)
-
-                // Image creation
-                //val imageResponse = generateImage(client, messageJSON.getString("recipe_name"))
-                //val imageResponseURL = JSONObject(imageResponse).getJSONArray("data").getJSONObject(0).getString("url")
-
-                // Standard image
-                val imageResponseURL = "https://cdn.discordapp.com/attachments/1148561836724207708/1172157068497666048/image.png?ex=655f4b56&is=654cd656&hm=a296565e26720c460d137ee7941dd195e597378e26b6e77cc7d1320551067ad0&"
-
-                val recipes = listOf(
-                    Recipe(
-                        "yh",
-                        messageJSON.getString("recipe_name"),
-                        R.drawable.food,
-                        imageResponseURL,
-                        messageJSON.getString("recipe_time"),
-                        false,
-                        messageJSON.getString("recipe_instructions"),
-                        messageJSON.getString("recipe_nutrition"),
-                        messageJSON.getString("recipe_ingredients"),
-                        user!!.uid
-                    )
-                )
-                return@withContext recipes
-                }
-                catch (exception: JSONException){
-                    Log.e("ChatCompletionError", "Error parsing chat completion to JSON: ${exception.message}")
-                }
-            }
+        catch (exception: IOException){
+            Log.e("ParseError", "Error parsing time to int: ${exception.message}")
+            println("Ingredients list empty or time is invalid")
+            val defaultRecipe = Recipe(
+                "uh",
+                "Failed tomato soup",
+                R.drawable.food,
+                "https://cdn.discordapp.com/attachments/1148561836724207708/1172151716741906503/image.png?ex=655f465a&is=654cd15a&hm=2ee66b50819a6faa6c8b4e3afa638b5540f1cd59f386703b10b40609ac7645a4&",
+                "60",
+                false,
+                "Something failed",
+                "{\"calories\":0,\"protein\":0,\"carbohydrates\":0,\"fat\":0}",
+                "[\"Failure\"]",
+                user!!.uid
+            )
+            return@withContext listOf(defaultRecipe)
         }
-        else{
-            println(response.message)
-            println(response)
-            println(response.body)
-        }
-
-        // Log error and return default failed recipe
-        Log.e("ChatCompletionError", "Error generating recipe")
-        val defaultRecipe = Recipe(
-            "uh",
-            "Failed tomato soup",
-            R.drawable.food,
-            "https://cdn.discordapp.com/attachments/1148561836724207708/1172151716741906503/image.png?ex=655f465a&is=654cd15a&hm=2ee66b50819a6faa6c8b4e3afa638b5540f1cd59f386703b10b40609ac7645a4&",
-            "60",
-            false,
-            "Something failed",
-            "{\"calories\":0,\"protein\":0,\"carbohydrates\":0,\"fat\":0}",
-            "[\"Failure\"]",
-            user!!.uid
-        )
-        return@withContext listOf(defaultRecipe)
     }
 
     suspend fun generateImage(client: OkHttpClient, recipeName: String): String {
