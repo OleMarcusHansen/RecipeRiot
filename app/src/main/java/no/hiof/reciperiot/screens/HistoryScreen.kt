bbc97523d4b09bced82c146e1c9d150bd206ea60
tsvar <galiomain@outlook.com>
2023-11-17 13:47:52 +0100

    search bar er nå implementert

--- app/src/main/java/no/hiof/reciperiot/screens/HistoryScreen.kt
+++ app/src/main/java/no/hiof/reciperiot/screens/HistoryScreen.kt
@@ -1,21 +1,46 @@
 package no.hiof.reciperiot.screens
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.google.firebase.firestore.FirebaseFirestore
 import no.hiof.reciperiot.ViewModels.RecipeViewModel
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HistoryScreen(navController: NavController, db: FirebaseFirestore, RecipeViewModel: RecipeViewModel = viewModel()) {
-    RecipeList(
-        recipes = RecipeViewModel.history,
-        navController = navController,
-        onFavouriteToggle = {
-        },
-        updateRecipeFavouriteStatus = { recipe, fav ->
-            RecipeViewModel.updateRecipeFavouriteStatus(recipe, db, fav)
-        }
+    var searchText by remember { mutableStateOf("") }
 
-    )
+    Column {
+        TextField(
+            value = searchText,
+            onValueChange = { newText ->
+                searchText = newText
+            },
+            label = { Text("Search") },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp))
+
+        RecipeList(
+            recipes = RecipeViewModel.history.filter { it.title.contains(searchText, true) },
+            navController = navController,
+            onFavouriteToggle = {},
+            updateRecipeFavouriteStatus = { recipe, fav ->
+                RecipeViewModel.updateRecipeFavouriteStatus(recipe, db, fav)
+            }
+        )
+    }
 }
