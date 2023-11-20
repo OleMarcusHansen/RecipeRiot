12e2b33f978d8fb6b08043f169a18cf1c6c8df4e
Ole Marcus Hansen <omhanse@hiof.no>
2023-11-20 13:22:03 +0100

    Refactor ingredients logic to view model and repository

--- app/src/main/java/no/hiof/reciperiot/ViewModels/IngredientsViewModel.kt
+++ app/src/main/java/no/hiof/reciperiot/ViewModels/IngredientsViewModel.kt
@@ -5,6 +5,7 @@ import android.content.ContentValues
 import android.util.Log
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
@@ -13,45 +14,41 @@ import com.google.firebase.firestore.FieldValue
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import no.hiof.reciperiot.data.IngredientsRepository
 
 class IngredientsViewModel : ViewModel() {
-    private val db = Firebase.firestore
-    private val user = Firebase.auth.currentUser
-
     var newIngredient by mutableStateOf("")
     var ingredientsList by mutableStateOf(emptyList<Pair<String, MutableState<Boolean>>>())
 
+    private val ingredientRepository = IngredientsRepository()
+
     fun deleteIngredient(ingredientName: String) {
-
-        val docRef = user?.let { db.collection("ingredients").document(it.uid) }
-
-        // Delete the specific field from the document
-        docRef?.update(ingredientName, FieldValue.delete())
-            ?.addOnSuccessListener {
-                Log.d(ContentValues.TAG, "DocumentSnapshot successfully deleted!")
-            }
-            ?.addOnFailureListener { e ->
-                Log.w(ContentValues.TAG, "Error deleting document", e)
-            }
+        ingredientRepository.deleteIngredient(ingredientName)
+        updateIngredientsList()
     }
-    fun saveIngredientsToDb(ingredientList: List<Pair<String, Boolean>>) {
+
+    fun saveIngredientsToDb() {
         //TODO: ensure logged in
-        val docRef = user?.let { db.collection("ingredients").document(it.uid) }
-        val data = hashMapOf<String, Any>()
-        for ((name, checked) in ingredientList) {
-            data[name] = checked
-        }
-        //TODO: ensure logged in
-        if (docRef != null) {
-            docRef.set(data)
-                .addOnSuccessListener { docRef ->
-                    Log.d(ContentValues.TAG, "DocumentSnapcshot added!")
-                }
-                .addOnFailureListener { e ->
-                    Log.w(ContentValues.TAG, "Error adding document", e)
-                }
+        val ingredientsToSave =
+            ingredientsList.map { (name, checkedState) ->
+                name to checkedState.value
+            }
+        ingredientRepository.saveIngredientsToDb(ingredientsToSave)
+    }
+
+    fun updateIngredientsList() {
+        ingredientRepository.fetchIngredients() { data ->
+            if (data != null) {
+                val firestoreIngredients =
+                    data.entries.map { it.key to mutableStateOf(it.value as Boolean) }
+                ingredientsList = firestoreIngredients
+            } else {
+                println("No data or error")
+            }
         }
     }
+
+    /*
     fun getIngredientsToIngredientScreen(callback: (Map<String, Any>?) -> Unit) {
         // TODO: Ensure logged in
         val docRef = user?.let { db.collection("ingredients").document(it.uid) }
@@ -86,5 +83,5 @@ class IngredientsViewModel : ViewModel() {
             }
         }
     }
-
+    */
 }
--- app/src/main/java/no/hiof/reciperiot/data/IngredientsRepository.kt
+++ app/src/main/java/no/hiof/reciperiot/data/IngredientsRepository.kt
@@ -1,5 +1,7 @@
 package no.hiof.reciperiot.data
 
+import android.content.ContentValues
+import android.util.Log
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import com.google.firebase.Firebase
@@ -13,9 +15,69 @@ class IngredientsRepository {
     private val db = Firebase.firestore
     private val user = Firebase.auth.currentUser
 
-    val ingredientsList = mutableStateOf(emptyList<Pair<String, MutableState<Boolean>>>())
+    //val ingredientsList = mutableStateOf(emptyList<Pair<String, MutableState<Boolean>>>())
 
 
+
+
+    fun fetchIngredients(callback: (Map<String, Any>?) -> Unit) {
+        // TODO: Ensure logged in
+        val docRef = user?.let { db.collection("ingredients").document(it.uid) }
+        docRef?.get()?.addOnSuccessListener { document ->
+            if (document != null && document.exists()) {
+                callback(document.data)
+            } else {
+                val emptyData = emptyMap<String, Any>()
+                docRef.set(emptyData)
+                    ?.addOnSuccessListener {
+                        callback(emptyData)
+                    }
+                    ?.addOnFailureListener { exception ->
+                        Log.d(ContentValues.TAG, "Error: Could not create doc", exception)
+                        callback(null)
+                    }
+            }
+        }?.addOnFailureListener { exception ->
+            Log.d(ContentValues.TAG, "get failed with ", exception)
+            callback(null)
+        }
+    }
+
+    fun deleteIngredient(ingredientName: String) {
+
+        val docRef = user?.let { db.collection("ingredients").document(it.uid) }
+
+        // Delete the specific field from the document
+        docRef?.update(ingredientName, FieldValue.delete())
+            ?.addOnSuccessListener {
+                Log.d(ContentValues.TAG, "DocumentSnapshot successfully deleted!")
+            }
+            ?.addOnFailureListener { e ->
+                Log.w(ContentValues.TAG, "Error deleting document", e)
+            }
+    }
+
+    fun saveIngredientsToDb(ingredientList: List<Pair<String, Boolean>>) {
+        //TODO: ensure logged in
+        val docRef = user?.let { db.collection("ingredients").document(it.uid) }
+        val data = hashMapOf<String, Any>()
+        for ((name, checked) in ingredientList) {
+            data[name] = checked
+        }
+        //TODO: ensure logged in
+        if (docRef != null) {
+            docRef.set(data)
+                .addOnSuccessListener { docRef ->
+                    Log.d(ContentValues.TAG, "DocumentSnapcshot added!")
+                }
+                .addOnFailureListener { e ->
+                    Log.w(ContentValues.TAG, "Error adding document", e)
+                }
+        }
+    }
+
+
+    /*
     suspend fun getIngredients() {
         val docRef = user?.let { db.collection("ingredients").document(it.uid) }
         val data = docRef?.get()?.await()?.data
@@ -53,6 +115,6 @@ class IngredientsRepository {
 
         docRef?.set(data)?.await()
     }
-
+    */
 
 }
